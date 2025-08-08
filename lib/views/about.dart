b89b0ce662a0188e9088d12c9554cfd180679f80
Debanjan <debanjannaskar1@gmail.com>
2025-08-08 12:14:32 +0530

    chore: About Page configured

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -7,7 +7,7 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("I am Debanajan"),
+      child: Text("To be Added"),
     );
   }
 }
