d983cb159f73a2386081bd9e33d9b496cb2ab411
Debanjan <debanjannaskar1@gmail.com>
2025-08-11 16:13:23 +0530

    feat: Rotation control improved

--- lib/controllers/robots/arm_controller.dart
+++ lib/controllers/robots/arm_controller.dart
@@ -6,7 +6,7 @@ import 'package:robosim/controllers/controls.dart'; // Import joystick package
 
 class ArmController extends GetxController {
   final ControlsController controlsController = Get.find();
-  double angle = 0.0;
+  double rotVel = 0.0;
   double length = 0.0;
   double height = 0.0;
   double gripper = 0.0;
@@ -14,12 +14,14 @@ class ArmController extends GetxController {
   void onRotationChanged(StickDragDetails details, String address) {
       // You can implement your logic here.
       // For example, sending data through the WebSocket.
-      double x = details.x;
-      double y = details.y;
-      if (x == 0.0 && y == 0.0) return;
-      angle = atan2(x, -y);
-      print('Joystick 1: x=${x.toStringAsFixed(2)}, y=${y.toStringAsFixed(2)}, angle=${angle.toStringAsFixed(2)}');
-      controlsController.sendJointPositions("/joystick_jacobian", [angle, 0.0, 0.0, gripper]);
+      // double x = details.x;
+      // double y = details.y;
+      // if (x == 0.0 && y == 0.0) return;
+      // rotVel = atan2(x, -y);
+      // print('Joystick 1: x=${x.toStringAsFixed(2)}, y=${y.toStringAsFixed(2)}, rotVel=${rotVel.toStringAsFixed(2)}');
+      rotVel = details.x;
+      print('Joystick 1: x=${rotVel.toStringAsFixed(2)}');
+      controlsController.sendJointPositions("/joystick_jacobian", [rotVel, length, -height, gripper]);
       // Example: sendDataToRobot('j1:${x.toStringAsFixed(2)},${y.toStringAsFixed(2)}');
     }
 
@@ -30,18 +32,18 @@ class ArmController extends GetxController {
       length = details.x;
       height = details.y;
       // if (x == 0.0 && y == 0.0) return;
-      controlsController.sendJointPositions("/joystick_jacobian", [angle, length, -height, gripper]);
+      controlsController.sendJointPositions("/joystick_jacobian", [rotVel, length, -height, gripper]);
       print('Joystick 2: x=${length.toStringAsFixed(2)}, y=${height.toStringAsFixed(2)}');
       // Example: sendDataToRobot('j2:${x.toStringAsFixed(2)},${y.toStringAsFixed(2)}');
     }
 
     void onGripped() {
       gripper = 0.22;
-      controlsController.sendJointPositions("/joystick_jacobian", [angle, length, -height, gripper]);
+      controlsController.sendJointPositions("/joystick_jacobian", [rotVel, length, -height, gripper]);
     }
 
     void onReleased() {
       gripper = 0.7;
-      controlsController.sendJointPositions("/joystick_jacobian", [angle, length, -height, gripper]);
+      controlsController.sendJointPositions("/joystick_jacobian", [rotVel, length, -height, gripper]);
     }
 }
